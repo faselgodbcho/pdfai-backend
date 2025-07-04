70124d3f091a25e25c2ce9137f1ad507715f0a0c
Fasel Godbcho <faselgodbcho@gmail.com>
2025-07-04 10:43:09 +0300

    Fix CSRF_TRUSTED_ORIGINS missing origin

--- pdfai/settings.py
+++ pdfai/settings.py
@@ -169,6 +169,8 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
 
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
+
 if DEBUG and not CORS_ALLOWED_ORIGINS:
     CORS_ALLOWED_ORIGINS += ["http://localhost:3000",
                              "https://localhost:3000",
