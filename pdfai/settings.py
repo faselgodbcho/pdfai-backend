26162a2d634bfc896791119a045a6dc7731d7e7c
Fasel Godbcho <faselgodbcho@gmail.com>
2025-07-04 09:46:26 +0300

    Remove unnecessary text nodes.

--- pdfai/settings.py
+++ pdfai/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import datetime
 from pathlib import Path
 from environ import Env
-from environ import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,10 +92,9 @@ WSGI_APPLICATION = 'pdfai.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
