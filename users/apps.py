8751bebbb1acb7c99fadb643c692a8570e779459
Fasel Godbcho <faselgodbcho@gmail.com>
2025-07-04 10:54:37 +0300

    Create superuser.

--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,15 @@
+import logging
+from django.contrib.auth import get_user_model
 from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+        User = get_user_model()
+        if not User.objects.filter(is_superuser=True).exists():
+            User.objects.create_superuser(
+                'fasel', 'fasel@gmail.com', 'i_love_dogs')
+            logging.info("Superuser created automatically")
